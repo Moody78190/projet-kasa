719682f7b33143c724d0c8a12b5cb1c8dc2337b8
Redwan <moody.d78190>
2024-03-24 17:58:11 +0100

    media queries implemented

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -2,40 +2,6 @@ import React, { useState } from 'react';
 import arrowTop from '../Assets/images/arrow_top.png';
 import arrowDown from '../Assets/images/arrow_down.png'
 
-// const Collapse = ({ menus, containerName = "Collapse-container", buttonClassName = "collapsible", buttonName }) => {
-//   const [menuStates, setMenuStates] = useState(
-//     Object.fromEntries(menus.map(menu => [menu.id, false]))
-//   );
-
-//   const toggleMenu = (menuId) => {
-//     setMenuStates({
-//       ...menuStates,
-//       [menuId]: !menuStates[menuId]
-//     });
-//   };
-
-//   return (
-//     <main>
-//       <div className={containerName}>
-//         {menus.map((menu, index) => (
-//           <div key={`${containerName}-${menu.id}-${index}`}>
-//             <button className={buttonClassName}>
-//               {buttonName ? buttonName : menu.title}
-//               <img className='arrow_top' onClick={() => toggleMenu(menu.id)} src={arrow_top} alt="arrow_top" style={{ transform: menuStates[menu.id] ? 'rotate(180deg)' : 'rotate(0deg)' }} />
-//             </button>
-//             <div className={menuStates[menu.id] ? 'content active' : 'content'}>
-//               <p>{menu.content}</p>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </main>
-//   );
-// };
-
-
-// export default Collapse;
-
 const Collapse = ({ item }) => {
   const [isOpen, setIsOpen] = useState(false)
 
@@ -58,7 +24,7 @@ const Collapse = ({ item }) => {
       <div className="dropdown">
         <span>{item.title}</span>
         <img
-          src={isOpen ? arrowTop : arrowDown}
+          src={isOpen ? arrowDown: arrowTop}
           alt="arrow"
           onClick={() => setIsOpen(!isOpen)}
         />
